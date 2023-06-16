06fee8dc088a0c14464dda17d92a4132fc4fb32a
Chattox <conradlineker@gmail.com>
2023-06-16 15:27:36 +0100

    feat: time display

--- src/components/Display/index.tsx
+++ src/components/Display/index.tsx
@@ -1,11 +1,13 @@
 import { Flex } from '@mantine/core';
 import { PiStream } from '../PiStream';
 import { Temperature } from '../Temperature';
+import { Clock } from '../Clock';
 
 export const Display = () => {
   return (
     <Flex direction="column" align="center" justify="center" gap="xl">
       <PiStream />
+      <Clock />
       <Temperature />
     </Flex>
   );

5dc2c4efe2ee701b463a4420632a4be25d57ea31
Chattox <conradlineker@gmail.com>
2023-06-23 23:18:19 +0100

    fix(temp): timestamp always one month out

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,6 +1,6 @@
 export const formatTime = (time: Date) => {
   const day = time.getDate().toString().padStart(2, '0');
-  const month = time.getMonth().toString().padStart(2, '0');
+  const month = (time.getMonth() + 1).toString().padStart(2, '0');
   const year = time.getFullYear().toString().slice(-2);
   const hours = time.getHours().toString().padStart(2, '0');
   const minutes = time.getMinutes().toString().padStart(2, '0');
